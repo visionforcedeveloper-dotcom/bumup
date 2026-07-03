0ca249d45a2c64bda037eb8ab4cc652b07c76fd3
BumUp <bumup@app.com>
2026-07-03 13:25:51 -0300

    Fix babel.config.js - replace worklets plugin with reanimated plugin

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // reanimated v4 usa worklets plugin
-      'react-native-worklets/plugin',
+      'react-native-reanimated/plugin',
     ],
   };
 };
